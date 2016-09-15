cf82a0757e4d5c103ea0f1da321bc0cb5d2655e7
Timothée Lottaz <timozattol@gmail.com>
2016-09-15 13:57:58 +0200

    Rename world_manager

--- src/menu.py
+++ src/menu.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
-"""Real-Rush main module"""
+"""Real-Rush menu"""
 
-import os
 import pygame
-
-from rush_game import RushGame
-from player import Player
-from block import Block
-from level_01 import Level01
-import colors
-import constants
 
 BG_MOON = pygame.image.load("../assets/png/layers/parallax-mountain-bg.png")
 BG_MOON_RES = BG_MOON.get_rect().size
@@ -25,16 +17,16 @@ BG_TREES_FAR_RES = BG_TREES_FAR.get_rect().size
 
 BG_TREES_FRONT = pygame.image.load("../assets/png/layers/parallax-mountain-foreground-trees.png")
 BG_TREES_FRONT_RES = BG_TREES_FRONT.get_rect().size
-class WorldManager():
-    def __init__(self,player,display):
+class Menu():
+    def __init__(self, player, display):
         self.player = player
         self.display = display
         # Background moon
         self.bg_moon = self._load_bg_images(BG_MOON,BG_MOON_RES)
         # Background mountains far
-        self.bg_mountains_far = self._load_bg_images(BG_MOUNTAINS_FAR,BG_MOUNTAINS_FAR_RES)        
+        self.bg_mountains_far = self._load_bg_images(BG_MOUNTAINS_FAR,BG_MOUNTAINS_FAR_RES)
         #Background mountains
-        self.bg_mountains = self._load_bg_images(BG_MOUNTAINS,BG_MOUNTAINS_RES)        
+        self.bg_mountains = self._load_bg_images(BG_MOUNTAINS,BG_MOUNTAINS_RES)
         #Background trees
         self.bg_trees_far = self._load_bg_images(BG_TREES_FAR,BG_TREES_FAR_RES)
         #Foreground trees
@@ -54,8 +46,8 @@ class WorldManager():
         self.display.blit(self.bg_moon, (0, 0))
         self.display.blit(self.bg_mountains_far, (0, 0))
         self.display.blit(self.bg_mountains, (-self.bg_offset, 0))
-        self.display.blit(self.bg_trees_far, (-self.tree_far_offset,0))
-        self.display.blit(self.bg_trees_front, (-self.tree_front_offset,0))
+        self.display.blit(self.bg_trees_far, (-self.tree_far_offset, 0))
+        self.display.blit(self.bg_trees_front, (-self.tree_front_offset, 0))
 
         self.bg_offset += self.mountains_speed * elapsed_time
         self.tree_far_offset += self.trees_far_speed * elapsed_time
@@ -66,11 +58,10 @@ class WorldManager():
         self.tree_front_offset %= self.bg_trees_front.get_width()
 
 
-    def _load_bg_images(self,image,res):
-        ret = pygame.Surface(res,pygame.SRCALPHA,32)
+    def _load_bg_images(self, image,res):
+        ret = pygame.Surface(res, pygame.SRCALPHA, 32)
         ret = ret.convert_alpha()
         ret.blit(image, (0, 0))
         ratio = self.display.get_size()[1] / res[1]
         ret = pygame.transform.scale(image, (int(res[0] * ratio), int(res[1] * ratio)))
         return ret
-
