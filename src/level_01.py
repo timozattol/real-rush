30deffa9e362f593d3cc6feffeb58ce1e3d8c45d
Timothée Lottaz <timozattol@gmail.com>
2016-09-15 18:57:22 +0200

    City tests

--- src/level_01.py
+++ src/level_01.py
@@ -5,9 +5,13 @@ import colors
 from level import Level
 from prefabs import BlockPrefab
 from constants import BG_SCROLL_SPEED
+from utils import load_bg_images_scale_y
 
-BG_IMAGE = pygame.image.load("../assets/png/city-background.png")
-BG_RESOLUTION = BG_IMAGE.get_rect().size
+BG = pygame.image.load("../assets/png/city-background-tests.png")
+BG_RES = BG.get_rect().size
+
+BG_FAR = pygame.image.load("../assets/png/cemetary.png")
+BG_FAR_RES = BG_FAR.get_rect().size
 
 class Level01(Level):
     def __init__(self, player, display):
@@ -42,11 +46,15 @@ class Level01(Level):
             self.platform_blocks.add(sprite)
 
         # Background
-        self.bg = pygame.Surface(BG_RESOLUTION)
-        self.bg = self.bg.convert()
-        self.bg.blit(BG_IMAGE, (0, 0))
-        ratio = self.display.get_size()[1] / BG_RESOLUTION[1]
-        self.bg = pygame.transform.scale(self.bg, (int(BG_RESOLUTION[0] * ratio), int(BG_RESOLUTION[1] * ratio)))
+        self.bg = load_bg_images_scale_y(BG, BG_RES, self.display)
+
+        ratio = 0.7
+        ret = pygame.Surface(BG_FAR_RES, pygame.SRCALPHA, 32)
+        ret = ret.convert_alpha()
+        ret = pygame.transform.scale(BG_FAR, (int(ratio * BG_FAR_RES[0]), int(ratio * BG_FAR_RES[1])))
+        self.bg_far = ret
+
+        # Scrolling parameter
         self.bg_offset = 0.0
 
         self.speed = BG_SCROLL_SPEED
@@ -62,6 +70,7 @@ class Level01(Level):
                 self.speed -= 10.0
 
         # Scroll and blit background
+        self.display.blit(self.bg_far, (-350, 0))
         self.display.blit(self.bg, (-self.bg_offset, 0))
         self.display.blit(self.bg, (self.bg.get_width() - self.bg_offset, 0))
         self.bg_offset += self.delta_pos(elapsed_time)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import pygame
+
 from constants import WINDOW_SIZE, FLOOR_HEIGHT, TILE_WIDTH, TILE_HEIGHT, PLAYER_LEFT_OFFSET
 
 def pos_convert(pos):
@@ -6,3 +8,10 @@ def pos_convert(pos):
     y = WINDOW_SIZE[1] - FLOOR_HEIGHT - pos[1] * TILE_HEIGHT
 
     return (x, y)
+
+def load_bg_images_scale_y(image, res, display):
+    ret = pygame.Surface(res, pygame.SRCALPHA, 32)
+    ret = ret.convert_alpha()
+    ratio = display.get_size()[1] / res[1]
+    ret = pygame.transform.scale(image, (int(res[0] * ratio), int(res[1] * ratio)))
+    return ret
